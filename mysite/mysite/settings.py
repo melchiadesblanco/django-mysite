cba379a69a3e79e4d81c12da3c79e5265f242788
Melchiades <melchiades.blanco@live.com>
2020-04-04 11:56:06 -0300

    saturdays commit
    here we go

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Thrid Party Apps
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
